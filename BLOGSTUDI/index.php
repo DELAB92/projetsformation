936003153a804bba16a886226983d9e664da596e
DELAB92 <144733706+DELAB92@users.noreply.github.com>
2023-09-17 14:28:42 +0200

    Add files via upload

---
+++ BLOGSTUDI/index.php
@@ -0,0 +1,74 @@
+    <?php
+    require_once __DIR__."/templates/header.php";
+    ?>
+
+    
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="assets/images/logo-tech-trendz.png" class="d-block mx-lg-auto img-fluid" alt="logo tech-trendz" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">SAMY LE ROI DE JEANNE D'ARC</font>
+          </font>
+        </h1>
+        <p class="lead">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">SI VOUS VOYEZ SAMY VOUS DEVEZ LE SALUEZ. LE RESPECT EST LA BASE DE LA VIE EN SOCIETE.</font>
+          </font>
+        </p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Primaire</font>
+            </font>
+          </button>
+          <button type="button" class="btn btn-outline-secondary btn-lg px-4">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Défaut</font>
+            </font>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="row text-center">
+      <div class="col-md-4 my-2">
+        <div class="card">
+          <img src="uploads\articles\1-php-vs-python.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 my-2">
+        <div class="card">
+          <img src="uploads\articles\1-php-vs-python.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 my-2">
+        <div class="card">
+          <img src="uploads\articles\1-php-vs-python.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <?php
+        require_once __DIR__. "/templates/footer.php";
+    ?>
---
+++ BLOGSTUDI/templates/footer.php
@@ -0,0 +1,43 @@
+</main>
+
+<footer class="py-3 my-4">
+  <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;">Maison</font>
+        </font>
+      </a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;">Caractéristiques</font>
+        </font>
+      </a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;">Tarifs</font>
+        </font>
+      </a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;">FAQ</font>
+        </font>
+      </a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;">À propos</font>
+        </font>
+      </a></li>
+  </ul>
+  <p class="text-center text-body-secondary">
+    <font style="vertical-align: inherit;">
+      <font style="vertical-align: inherit;">© 2023 Société, Inc.</font>
+    </font>
+  </p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
